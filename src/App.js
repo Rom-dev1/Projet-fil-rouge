7e9217f09d9ad605745ed8fa8b93297ef14753c0
Romain Trochu <romain.trochu@gmail.com>
2023-02-20 22:42:51 +0100

    init projet

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./component/header";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className="max-w-xl mx-auto mt-4">
+        <h1 className="text-3xl font-bold underline">Hello</h1>
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/component/header.js
@@ -0,0 +1,27 @@
+import img from '../img/network.jpg'
+import logo from '../img/treeBlack.jpg'
+export default function Header(){
+
+    return(
+        <>
+        <img src={img} className="absolute -z-10 w-full opacity-10" alt="" />
+        <div className="flex justify-between flex-row content-center p-4 w-3/4 m-auto mt-6 text-xl font-bold">
+           
+           <div className='flex items-center  space-x-5'>
+                <img src={logo} className="w-20" alt="" />
+                <h2>[IT]car.net</h2>
+            </div>
+            <div className='flex items-center'>
+                <nav>
+                    <ul className="space-x-10 flex">
+                        <li>Entreprise</li>
+                        <li>Organisme de formation</li>
+                        <li>Candidat</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </div></>
+        
+    )
+}
