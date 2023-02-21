f0f17355759b2ebbe8121e99e53fc015387d13a9
Romain Trochu <romain.trochu@gmail.com>
2023-02-21 22:06:07 +0100

    add content

---
+++ src/component/Candidat.jsx
@@ -0,0 +1,14 @@
+export default function Candidat(){
+    return(
+        <>  <div >
+                <div className="flex w-3/4 mx-auto mt-4 p-10 text-2xl items-center justify-between h-1/2">
+                    <p className="w-1/2">A toi jeune stagiaire, perdu et égaré entre toutes ces ESN qui n'aspirent qu'a trouver l'aspirant idéal, sert toi de notre formidable site pour chercher, trouver le bonheur et le graal qu'est le STAGE</p>
+                    <div  className="w-1/3 flex text-center justify-center">
+                        <span className="bg-red-500 w2/3 px-10 py-5 rounded-lg">Candidat</span>
+                    </div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/component/Content_info.jsx
@@ -0,0 +1,19 @@
+import desk from '../img/Desk.jpg'
+
+export default function ContentInfo(){
+    return(
+        <>  
+            <div className=" flex justify-between w-3/4 mx-auto mt-4">
+                
+                <div className="flex flex-col w-1/3 space-y-5" >
+                    <h1 className='text-3xl font-bold'>CMRC, au coeur de l'IT</h1>
+                    <p>Notre équipe, au nombre extraordinaire de 4 personnes, dédiée à vos besoin met tout en oeuvre afin de répondre à vos besoins. Que vous soyez stagiaire, professionnel du secteur ou un organisme de formation, n'ayez crainte, on est la! </p>
+                    <span className='text-xl'>Suivez le guide!</span>
+                </div>
+                <div className='w-1/2'>
+                    <img src={desk} className="w-full" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
