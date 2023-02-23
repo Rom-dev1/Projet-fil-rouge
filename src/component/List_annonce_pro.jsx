0f9159aef8a7cda6d347c0e288863ab785271f67
Romain Trochu <romain.trochu@gmail.com>
2023-02-23 16:09:05 +0100

    page annonce pro

---
+++ src/component/List_annonce_pro.jsx
@@ -0,0 +1,30 @@
+export default function List_annonce_pro(){
+    return(
+        <>
+        <div className="text-center">
+            <h1 className="text-3xl font-bold">LISTE DES ANNONCES DES PRO ET DES ORGANISMES DE FORMATION</h1>
+        </div>
+        <div className="flex w-3/4 mx-auto space-y-8 mt-4 p-10 text-2xl items-center flex-col">
+            <div className="border-2 px-2 py-8 rounded bg-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi quaerat totam rem fuga velit et nihil, iste, recusandae commodi, dicta cumque cupiditate doloremque consequuntur tempore eum porro voluptatibus ad?</p>
+            </div>
+            <div className="border-2 px-2 py-8 rounded bg-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi quaerat totam rem fuga velit et nihil, iste, recusandae commodi, dicta cumque cupiditate doloremque consequuntur tempore eum porro voluptatibus ad?
+                </p>
+            </div>
+            <div className="border-2 px-2 py-8 rounded bg-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi quaerat totam rem fuga velit et nihil, iste, recusandae commodi, dicta cumque cupiditate doloremque consequuntur tempore eum porro voluptatibus add 
+                </p>
+            </div>
+            <div className="border-2 px-2 py-8 rounded bg-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi quaerat totam rem fuga velit et nihil, iste, recusandae commodi, dicta cumque cupiditate doloremque consequuntur tempore eum porro voluptatibus ad?
+                </p>
+            </div>
+            <div className="border-2 px-2 py-8 rounded bg-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nisi quaerat totam rem fuga velit et nihil, iste, recusandae commodi, dicta cumque cupiditate doloremque consequuntur tempore eum porro voluptatibus ad?
+                </p>
+            </div>
+        </div>
+        </>
+    )
+}
